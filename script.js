21e5bceb05ca306e248a56c08c65d2117f20e9a3
smith-stone <edogbanyaemmanuel3@gmail.com>
2022-10-05 22:53:11 +0100

    still in progress

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 let weather = { 
-    "apiKey" : "5bad05a158e5764a9cf3ed1c0be7aeb1"
-}
+    apiKey : "5bad05a158e5764a9cf3ed1c0be7aeb1",
+    fetchWeather: function (city) {
+        fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q=' 
+            + city 
+            + '&appid=' 
+            + this.apiKey
+            ).then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather;
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+    }
+};
